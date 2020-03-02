9d7ec08cba520c7f8e06fabde70c669a64b386fd
48lika48 <48lika48@mail.ru>
2020-03-02 12:37:53 +0300

    feat: add count-cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
-};
+    let concatArr = [];
+    let cats = [];
+    concatArr = [].concat(...matrix);
+    cats = concatArr.filter(function (item){
+      return item == "^^";
+    });
+    return cats.length;
+}
