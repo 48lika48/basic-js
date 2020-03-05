bc12d083c66026a2c0e9d0804c02fed85a3738bf
48lika48 <48lika48@mail.ru>
2020-03-05 15:40:54 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  //remove line with error and write your code here
+  if (Array.isArray(members) === false){ 
+    return false;
+  }
+
+  let newMembers = members.filter(function(item){
+    return typeof item == "string";
+  });
+  
+  let arr = newMembers.map(function(item){
+    if(item.charAt(0) === " ") {
+      return item.trim().slice(item.indexOf(" "), 1);
+    }
+    else {
+      return item.charAt(0);
+    }
+  });
+  
+  let str = arr.join("").toUpperCase();
+  let abbreviation = str.split("").sort().join("");
+
+  return abbreviation;
+}
