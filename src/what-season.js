7e009fa301c366d9f431af098c4dd2dbf8e141d5
48lika48 <48lika48@mail.ru>
2020-03-06 09:59:19 +0300

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,28 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(date) {
   // remove line with error and write your code here
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (Object.prototype.toString.call(date) !== "[object Date]"){
+    throw Error();
+  }
+
+  let month = date.getMonth();
+
+  if (typeof month !== "number") {
+    return error;
+  }
+  else if (month == 11 || month == 0 || month == 1) {
+    return "winter";
+  }
+  else if(month == 2 || month == 3 || month == 4) {
+    return "spring";
+  }
+  else if(month == 5 || month == 6 || month == 7) {
+    return "summer";
+  }
+  else /*if(month == 8 || month == 9 || month == 10)*/ {
+    return "autumn";
+  }
 };
