6c496cce1134aa2c70a6396d8d256724b1345b1e
48lika48 <48lika48@mail.ru>
2020-03-22 20:36:22 +0300

    feat: add not-full transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,43 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
     // remove line with error and write your code here
+    if(Array.isArray(arr) === false){ 
+        throw Error();
+    }
+    if(arr.length === 0){
+        return [];
+    }
+
+    if(arr[0] == "--double-prev" || arr[0] == "--discard-prev"){
+        arr.shift();
+    }
+    if(arr[arr.length - 1] == "--double-next" || arr[arr.length - 1] == "--discard-next"){
+        arr.pop();
+    }
+    
+    let double = [];
+    double = arr.map(function callback(currentValue, index){
+        if(currentValue == "--double-prev"){
+            return arr[index-1];
+        }
+        else if(currentValue == "--double-next"){
+            return arr[index+1];
+        }
+        else {
+            return currentValue;
+        } 
+    });
+
+    let discard = [];
+    discard = arr.map(function callback(currentValue, index){
+        if(currentValue == "--discard-prev"){
+            return arr.splice([index-1], 2);
+        }
+        else if(currentValue == "--discard-next"){
+            return arr.splice([index], 2);
+        }
+        else {
+            return currentValue;
+        }
+    });
+    return arr;
 };
